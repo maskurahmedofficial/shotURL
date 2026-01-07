ea0ea996681a8414077e08d95dbfa35d25a7a863
Maskur Ahmed <maskurahmed392@gmail.com>
2026-01-07 10:29:36 +0600

    cors

--- SHORT_URL_PROJECT/SHORT_URL_BACKEND/index.js
+++ SHORT_URL_PROJECT/SHORT_URL_BACKEND/index.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 9000
-const cors = require('cors')
-const  route  = require('./src/routes/routes')
-const morgan  = require('morgan');
-const { default: mongoose } = require('mongoose')
+const express = require('express');
+const app = express();
+const port = 9000;
+const cors = require('cors');
+const route = require('./src/routes/routes');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+// ---------- middleware ----------
+app.use(cors({
+  origin: "https://maskur-shorturl.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
-// ^------------ middlewere----------
+app.options("*", cors()); // ✅ REQUIRED
 
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(route);
 
-app.use(cors());
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
 
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(route)
+// ---------- database ----------
+mongoose.connect(
+  'mongodb+srv://Maskur:Maskur@cluster0.zn2onai.mongodb.net/Maskur?retryWrites=true&w=majority&appName=Cluster0'
+)
+.then(() => console.log('db connected'))
+.catch(err => console.log(err));
 
-app.get('/',(req,res)=>{
-res.send('server is running')
-})
-
-// *connecting database:
-mongoose.connect('mongodb+srv://Maskur:Maskur@cluster0.zn2onai.mongodb.net/Maskur?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=>console.log('db connected'))
-.catch((err)=>console.log(err))
-
-
-
-// &--------port listen----------
-app.listen(port,(err)=>{
-    if(err) return console.log(err)
-    console.log(`this port is running at ${port}`)
-})
+// ---------- port ----------
+app.listen(port, () => {
+  console.log(`this port is running at ${port}`);
+});
